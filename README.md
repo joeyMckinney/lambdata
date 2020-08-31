79c7f5b727d526cbddbe253317b4e34d2a01e5b5
joeymckinney <josiahmckinney321@yahoo.com>
2020-08-31 14:36:39 -0700

    adding more files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lambdata
 A Python package with some helpful Fata Science utilities
+
+
+________
+
+
+TODO - adding lots of cool functions and stuff!
---
+++ lambdata/__init__.py
@@ -0,0 +1,11 @@
+"""lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+TEST = pd.DataFrame(np.ones(10))
+
+def df_cleaner(df):
+    """Clean a dataframe!"""
+    pass # TODO -implement
