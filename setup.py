b12f4cb632789562175481e86c2c74a92cc92c75
joeymckinney <josiahmckinney321@yahoo.com>
2020-09-02 00:45:20 -0700

    added opp examples

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-jmckinney",
-    version="0.0.5",
+    version="0.0.7",
     author="jmckinney",
     description='A collection of Data Science help functions',
     long_description=LONG_DESCRIPTION,
     long_description_content='text/markdown',
     url="https://github.com/joeyMckinney/lambdata",
     packages=setuptools.find_packages(),
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     install_requires=REQUIRED,
     classifiers=[
         'Programming Language :: Python :: 3',
